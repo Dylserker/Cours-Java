39163cac65b09da1e19cfdfaa2fd4533b1e30490
Dylserker <levant.dylan@gmail.com>
2025-02-11 15:26:44 +0100

    push Method

--- Method/Exo3.java
+++ Method/Exo3.java
@@ -1,72 +1,30 @@
 public class Calculatrice {
 
-    public int add(int a, int b) {
-        return a + b;
-    }
+    public double add(int a, int b) { return a + b; }
+    public double sous(int a, int b) { return a - b; }
+    public double multi(int a, int b) { return a * b; }
+    public double divid(int a, int b) { return (double) a / b; }
 
-    public double add(double a, double b) {
-        return a + b;
-    }
-
-    public int sous(int a, int b) {
-        return a - b;
-    }
-
-    public double sous(double a, double b) {
-        return a - b;
-    }
-
-    public int multi(int a, int b) {
-        return a * b;
-    }
-
-    public double multi(double a, double b) {
-        return a * b;
-    }
-
-    public int divid(int a, int b) {
-        return a / b;
-    }
-
-    public double divid(double a, double b) {
-        return a / b;
-    }
+    public double add(double a, double b) { return a + b; }
+    public double sous(double a, double b) { return a - b; }
+    public double multi(double a, double b) { return a * b; }
+    public double divid(double a, double b) { return a / b; }
 
     public double effectuerOperation(String operation, double a, double b) {
-        double result = 0;
-
-        switch (operation) {
-            case "add":
-                result = add(a, b);
-                break;
-            case "sous":
-                result = sous(a, b);
-                break;
-            case "multi":
-                result = multi(a, b);
-                break;
-            case "divid":
-                result = divid(a, b);
-                break;
-            case "secret":
-                System.out.println("Easter Egg détecté ! Vous avez trouvé le secret.");
-                result = 42;
-                break;
-            default:
-                System.out.println("Opération inconnue.");
-        }
-
-        return result;
+        return switch (operation) {
+            case "add" -> add(a, b);
+            case "sous" -> sous(a, b);
+            case "multi" -> multi(a, b);
+            case "divid" -> divid(a, b);
+            case "secret" -> "easter egg".hashCode();
+            default -> throw new IllegalArgumentException("Opération inconnue");
+        };
     }
 
     public static void main(String[] args) {
         Calculatrice calc = new Calculatrice();
-
-        System.out.println("Addition (2 + 3): " + calc.effectuerOperation("add", 2, 3));
-        System.out.println("Soustraction (5 - 2): " + calc.effectuerOperation("sous", 5, 2));
-        System.out.println("Multiplication (4 * 6): " + calc.effectuerOperation("multi", 4, 6));
-        System.out.println("Division (10 / 2): " + calc.effectuerOperation("divid", 10, 2));
-
-        System.out.println("Opération secrète (Easter Egg): " + calc.effectuerOperation("secret", 0, 0));
+        System.out.println("Addition : " + calc.effectuerOperation("add", 5, 3));
+        System.out.println("Multiplication : " + calc.effectuerOperation("multi", 2.5, 4));
+        System.out.println("Easter Egg : " + calc.effectuerOperation("secret", 0, 0));
     }
 }
