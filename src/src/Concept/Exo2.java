b8e0c3df168095cd63062ee5444f3fff8f8e668b
Dylserker <levant.dylan@gmail.com>
2025-02-12 10:18:12 +0100

    push Concept

--- src/src/Concept/Exo2.java
+++ src/src/Concept/Exo2.java
@@ -1,4 +1,12 @@
 package Concept;
 
 public class Exo2 {
+    public static void main(String[] args) {
+        Voiture voiture = new Voiture("Tesla Model S", "Rouge", 79999);
+        voiture.afficherDetails();
+
+        voiture.setPrix(-5000);
+        voiture.setPrix(74999);
+        voiture.afficherDetails();
+    }
 }
